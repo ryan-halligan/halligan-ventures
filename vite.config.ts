fd47cb26e11e254f71237fd2a9fb8870fa34cd5e
ryan-halligan <rhalligan@gmail.com>
2025-11-24 20:55:22 -0500

    Update vite.config.ts
    Change base path to / for custom domain ryanhalligan.me

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: '/halligan-ventures/',
-  server: {
+  base: '/',
+    server: {
     host: "::",
     port: 8080,
   },
